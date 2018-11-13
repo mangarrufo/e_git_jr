00ce75c73d097e60b08acfbf6ec40d135a371022
jordi <jordi@gmail.com>
2018-11-13 09:40:07 +0100

    Primer commit

---
+++ prueba_git/src/prueba_git/prueba.java
@@ -0,0 +1,25 @@
+
+package prueba_git;
+
+import java.util.Scanner;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		int contador = 0;
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Hasta donde quieres que cuentes?");
+		while (!teclado.hasNextInt()) {
+			System.out.println("Tiene que ser un entero");
+			teclado.next();
+		}
+		contador = teclado.nextInt();
+		System.out.println("");
+		for (int i = 1; i <= contador; i++) {
+			System.out.print(i + " ");
+		}
+		teclado.close();
+	}
+
+}
